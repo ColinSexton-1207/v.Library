c27caafa0c713500b9f50ec3ace4abb9d8d15591
Colin Sexton <colin.sexton.1207@gmail.com>
2025-04-04 10:45:38 -0500

    Finished attaching carousel API to frontend... Seems to work appropriately. Spinner on page load is being funky, I might have to look into some manual delays upon page load to further test it. I can't tell if page is loading too quickly or spinner just doesn't appear... Updated several components to accomodate dynamic carousel data.

--- library-frontend/react-library/src/layouts/Home/components/Carousel.tsx
+++ library-frontend/react-library/src/layouts/Home/components/Carousel.tsx
@@ -1,8 +1,69 @@
 import { ReturnBook } from './ReturnBook';
 import { useEffect, useState } from 'react';
+import { Spinner } from '../../Utils/Spinner';
 import Book from '../../../models/BookModel';
 
 export const Carousel = () => {
+   const [books, setBooks] = useState<Book[]>([]);
+   const [isLoading, setIsLoading] = useState(true); // Display loading information
+   const [httpError, setHttpError] = useState(null); // Default API request errors to null
+
+   /* Integrate API Stuff */
+   useEffect(() => {
+      const fetchBooks = async () => {
+         const baseUrl: string = "http://localhost:8080/api/books";
+         const url: string = `${baseUrl}?page=0&size=9`; // Retrieve specific range of books w/ API call
+         const response = await fetch(url);
+
+         /* Error Response */
+         if (!response.ok) {
+            throw new Error('Error retrieving information...');
+         }
+
+         const responseJson = await response.json(); // Convert response data to JSON format
+         const responseData = responseJson._embedded.books; // Extracts data from responseJson object
+
+         const loadedBooks: Book[] = []; // Empty object
+
+         /* Basically BuildArrayList in work DAO */
+         for (const key in responseData) { // Delicious enhanced For-loop
+            loadedBooks.push({
+               id: responseData[key].id,
+               title: responseData[key].title,
+               author: responseData[key].author,
+               description: responseData[key].description,
+               copies: responseData[key].copies,
+               copiesAvailable: responseData[key].copiesAvailable,
+               category: responseData[key].category,
+               img: responseData[key].img,
+            });
+         }
+
+         setBooks(loadedBooks);
+         setIsLoading(false);
+      };
+      fetchBooks().catch((error: any) => {
+         setIsLoading(false);
+         setHttpError(error.message);
+      });
+   }, [/*variable*/]); // State variable - if anything changes, will call the API again
+
+   /* Loading Effects */
+   if (isLoading) {
+      return (
+         <Spinner/>
+      )
+   }
+
+   /* Error Effects */
+   if (httpError) {
+      return (
+         <div className='container m-5'>
+            <p>{httpError}</p>
+         </div>
+      )
+   }
+
    return (
       <div className='container mt-5' style={{ height: 550 }}>
          <div className='homepage-carousel-title'>
@@ -13,17 +74,23 @@ export const Carousel = () => {
             <div className='carousel-inner'>
                <div className='carousel-item active'>
                   <div className='row d-flex justify-content-center align-items-center'>
-                     <ReturnBook/>
+                     {books.slice(0, 3).map(book => (
+                        <ReturnBook book={book} key={book.id} />
+                     ))}
                   </div>
                </div>
                <div className='carousel-item'>
                   <div className='row d-flex justify-content-center align-items-center'>
-                     <ReturnBook/>
+                     {books.slice(3, 6).map(book => (
+                        <ReturnBook book={book} key={book.id} />
+                     ))}
                   </div>
                </div>
                <div className='carousel-item'>
                   <div className='row d-flex justify-content-center align-items-center'>
-                     <ReturnBook/>
+                     {books.slice(6, 9).map(book => (
+                        <ReturnBook book={book} key={book.id} />
+                     ))}
                   </div>
                </div>
                <button className='carousel-control-prev' type='button' data-bs-target='#carouselControls' data-bs-slide='prev'>
@@ -39,7 +106,7 @@ export const Carousel = () => {
          {/* Mobile */}
          <div className='d-lg-none mt-3'>
             <div className='row d-flex justifty-content-center align-items-center'>
-               <ReturnBook/>
+               <ReturnBook book = {books[3]} key = {books[3].id}/>
             </div>
          </div>
          <div className='homepage-carousel-title mt-3'>
--- library-frontend/react-library/src/layouts/Home/components/ReturnBook.tsx
+++ library-frontend/react-library/src/layouts/Home/components/ReturnBook.tsx
@@ -1,10 +1,15 @@
-export const ReturnBook = () => {
+import Book from './../../../models/BookModel';
+
+export const ReturnBook: React.FC<{book: Book}> = (props) => {
    return (
       <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
          <div className='text-center'>
-            <img src={require('./../../../Images/BooksImages/book1.jpg')} width='151' height='233' alt='Book' />
-            <h6 className='mt-2'>Book</h6>
-            <p>Book Title</p>
+            {/* If props.book.img is true (exists), return the img, else return the default img */}
+            {props.book.img ?
+               <img src={props.book.img} width='151' height='233' alt='Book' /> : <img src={require('./../../../Images/BooksImages/book1.jpg')} width='151' height='233' alt='Book' />
+            }
+            <h6 className='mt-2'>{props.book.title}</h6>
+            <p>{props.book.author}</p>
             <a className='btn main-color text-white' href='#'>Reserve</a>
          </div>
       </div>
